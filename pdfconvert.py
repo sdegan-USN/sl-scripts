8b12886449f950028dc08462d40b7be2d703337a
Dr. Sean Egan <sdegan@gmail.com>
2023-05-06 14:47:09 +0200

    Added pdfconvert.py and updated README

---
+++ pdfconvert.py
@@ -0,0 +1,9 @@
+import pdf2docx
+
+# Define input and output file paths
+pdf_file_path = "phys3.pdf"
+docx_file_path = "output.docx"
+
+# Convert PDF to DOCX
+pdf2docx.parse(pdf_file_path, docx_file_path)
+
